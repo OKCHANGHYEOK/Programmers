3b2b26b0fa702db9b6e5a44d65dec1ebdbc90f1b
OKCHANGHYEOK <145905698+OKCHANGHYEOK@users.noreply.github.com>
2023-10-29 17:25:33 +0900

    [level unrated] Title: 공백으로 구분하기 1, Time: 0.03 ms, Memory: 73.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
+++ 프로그래머스/unrated/181869. 공백으로 구분하기 1/공백으로 구분하기 1.java
@@ -7,27 +7,7 @@ class Solution {
             }
         }
         String[] answer = new String[size];
-        
-        if(my_string.contains(" ") == false) {
-            answer[0] = my_string;
-        }
-        else {
-            int k = 0;
-            int IsEmpty = 0;
-            for(int i = 0; i < my_string.length(); i++) {
-                if(my_string.charAt(i) == ' ') {
-                    answer[k] = my_string.substring(IsEmpty, i);
-                    answer[k] = answer[k].replace(" ", "");
-                    IsEmpty = i;
-                    k++;
-                }
-            }
-            
-            if(k < my_string.length()) {
-                answer[k] = my_string.substring(IsEmpty, my_string.length());
-                answer[k] = answer[k].replace(" ", "");
-            }
-        }
+        System.out.println(answer.length);
         
         return answer;
     }
