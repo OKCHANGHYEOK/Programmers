568df9faa37725f9abae3803f70b4ff8195db282
OKCHANGHYEOK <145905698+OKCHANGHYEOK@users.noreply.github.com>
2024-01-06 23:06:56 +0900

    [level 1] Title: 실패율, Time: 1.36 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,57 +1,54 @@
 import java.util.Arrays;
-import java.util.HashMap;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
     	Arrays.sort(stages);
-    	int size = stages[stages.length - 1] > N ? N : stages[stages.length - 1];
+    	int size = stages[stages.length - 1] > N ? stages[stages.length - 1] - 1 : N;
         int[] answer = new int[size];
         
-        // 각 스테이지별로 도전한 사람 수와 성공한 사람 수가 담긴 배열을 저장하는 HashMap
-        HashMap<Integer, Integer[]> hm = new HashMap<>();
-        for(int i = 1; i <= answer.length; i++) {
-           hm.put(i, new Integer[]{0, 0});
+        
+        
+        int[][] failure = new int[size][3];
+        for(int i = 0; i < failure.length; i++) {
+        	failure[i][0] = i + 1;
         }
         
         for(int i = 0; i < stages.length; i++) {
-           for(int key : hm.keySet()) {
-              if(stages[i] > key) {
-                 Integer[] count = hm.get(key);
-                 count[0]++;
-                 count[1]++;
-                 hm.put(key, count);
-              }
-              if(stages[i] == key) {
-                 Integer[] count = hm.get(key);
-                 count[0]++;
-              }
-           }
+        	for(int j = 0; j < failure.length; j++) {
+        		if(stages[i] > failure[j][0]) {
+        			failure[j][1]++;
+        			failure[j][2]++;
+        		}
+        		if(stages[i] == failure[j][0]) {
+        			failure[j][2]++;
+        		}
+        	}
         }
         
         
-        // 각 스테이지의 실패율 구하기
-        double[][] failure = new double[answer.length][3];
-        for(int i = 0; i < N; i++) {
-           for(int key : hm.keySet()) {
-              if(key - 1 == i) {
-                 failure[i][0] = i + 1;
-                 failure[i][1] = hm.get(key)[1];
-                 failure[i][2] = hm.get(key)[0];
-                 break;
-              }
-           }
-        }
+        Arrays.sort(failure, (a1, a2) -> comp(a1, a2));
         
-        Arrays.sort(failure, 
-        		(a1, a2) ->
-        		(int)((a1[1] * a2[2]) - (a2[1] * a1[2]))
-        		);
-        
+
         for(int i = 0; i < failure.length; i++) {
-        	answer[i] = (int)failure[i][0];
+        	answer[i] = failure[i][0];
         }
 
-        
+
         return answer;
     }
+    
+    // 성공률이 낮은(실패율이 높은)순으로 정렬하기 위해 사용할 비교자
+    public int comp(int[] a1, int[] a2) {
+    	int num = 0;
+    	
+    	double f1 = (double)a1[1] / a1[2];
+    	double f2 = (double)a2[1] / a2[2];
+    	
+    	if(f1 == f2) num = a1[0] - a2[0];
+    	else num = Double.compare(f1, f2);
+    	
+    	
+    	return num;
+    }
+    
 }
