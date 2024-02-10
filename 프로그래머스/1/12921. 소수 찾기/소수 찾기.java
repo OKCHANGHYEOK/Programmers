aa25e0622387639db97d3e93b20db5d868c6e6ca
OKCHANGHYEOK <145905698+OKCHANGHYEOK@users.noreply.github.com>
2024-02-10 23:01:55 +0900

    [level 1] Title: 소수 찾기, Time: 194.66 ms, Memory: 52.4 MB -BaekjoonHub

---
+++ 프로그래머스/1/12921. 소수 찾기/소수 찾기.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        
+        
+        for(int i = 2; i <= n; i++) {
+            boolean isSosu = true;
+            for(int j = 2; j <= Math.sqrt(i); j++) {
+                if(i % j == 0) {
+                    isSosu = false;
+                    break;
+                }
+            }
+            if(isSosu) answer++;
+        }
+        
+        
+
+        return answer;
+    }
+}
