e76c3272a38506d1ee0d5b4478f89455d7b9ec7c
OKCHANGHYEOK <145905698+OKCHANGHYEOK@users.noreply.github.com>
2024-05-26 14:51:12 +0900

    [level 2] Title: 피보나치 수, Time: 14.25 ms, Memory: 84 MB -BaekjoonHub

--- 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
+++ 프로그래머스/2/12945. 피보나치 수/피보나치 수.java
@@ -1,20 +1,28 @@
-import java.util.ArrayList;
-
 class Solution {
-    public long solution(int n) {
-        
-        ArrayList<Long> fibonacci = new ArrayList<Long>();
-        fibonacci.add(Long.valueOf(0));
-        fibonacci.add(Long.valueOf(1));
-        
-        while(fibonacci.size() != n + 1) {
-        	long n1 = fibonacci.get(fibonacci.size() - 1);
-        	long n2 = fibonacci.get(fibonacci.size() - 2);
-        	fibonacci.add((n1 + n2) % 1234567);
+    private int[] memo;
+    public int solution(int N) {
+        int answer = 0;
+
+        memo = new int[N + 1];
+
+        for (int i = 1; i <= N; i++) {
+            memo[i] = -1;
         }
-        
-        
-        
-        return fibonacci.get(fibonacci.size() - 1);
+        return fibonacci(N);
+    }
+
+    private int fibonacci(int n) {
+        if (memo[n] != -1) {
+            return memo[n];
+        }
+        if (n == 0) {
+            memo[n] = 0;
+            return 0;
+        }
+        if (n == 1) {
+            memo[n] = 1;
+            return 1;
+        }
+        return memo[n] = (fibonacci(n - 1) + fibonacci(n - 2)) % 1234567;
     }
 }
