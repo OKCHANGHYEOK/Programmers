d53b2edafda23e297a8a42e7a9f8012b6efdf2ed
OKCHANGHYEOK <145905698+OKCHANGHYEOK@users.noreply.github.com>
2024-12-17 20:45:57 +0900

    [level 0] Title: A 강조하기, Time: 0.73 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181874. A 강조하기/A 강조하기.cs
@@ -0,0 +1,12 @@
+using System;
+
+public class Solution {
+    public string solution(string myString) {
+        string answer = "";
+        
+        answer = myString.ToLower().Replace("a", "A");
+        
+        
+        return answer;
+    }
+}
