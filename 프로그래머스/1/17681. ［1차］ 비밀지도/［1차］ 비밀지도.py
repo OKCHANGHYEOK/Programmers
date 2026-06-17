d341cefbf2945f6cfdda2a680822e9a109ee4be9
OKCHANGHYEOK <145905698+OKCHANGHYEOK@users.noreply.github.com>
2026-06-17 15:07:13 +0900

    [level 1] Title: [1차] 비밀지도, Time: 0.02 ms, Memory: 11.4 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.py
@@ -1,10 +1,11 @@
 def solution(n, arr1, arr2):
     answer = []
 
+    trans = str.maketrans({'1' : '#', '0' : ' '})
+
     for i in range(0, n):
         curr = arr1[i] | arr2[i]
         binary = bin(curr)[2:].rjust(n, '0')
-        trans = str.maketrans({'1' : '#', '0' : ' '})
         answer.append(binary.translate(trans))
 
     return answer
