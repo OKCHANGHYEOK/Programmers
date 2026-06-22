9118b5fb471853de5ee38edcc1fd335228400ff1
OKCHANGHYEOK <145905698+OKCHANGHYEOK@users.noreply.github.com>
2026-06-22 13:56:35 +0900

    [level 1] Title: 추억 점수, Time: 4.27 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/1/176963. 추억 점수/추억 점수.py
+++ 프로그래머스/1/176963. 추억 점수/추억 점수.py
@@ -1,19 +1,14 @@
 def solution(name, yearning, photo):
     answer = []
-    
-    score_dict = {}
-    totalScore = 0
-    
-    for i in range(len(name)):   
-        score_dict[name[i]] = yearning[i]        
-    
-    for i in range(len(photo)):
-        totalScore = 0
-        for j in range(len(photo[i])):
-            score = score_dict.get(photo[i][j])
-            if(score != None):
-                totalScore += score       
-        answer.append(totalScore)
-    
-    
+
+    dict_score = dict(zip(name, yearning))
+
+    for arr in photo:
+        items = [dict_score[x] for x in arr if x in name]
+        
+        if items != []:
+            answer.append(sum(items))
+        else:
+            answer.append(0)    
+
     return answer
