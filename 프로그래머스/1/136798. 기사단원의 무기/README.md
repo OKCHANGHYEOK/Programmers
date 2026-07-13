1f692b8d4df8f7430ed8591ca2a848a16ed3c2ae
OKCHANGHYEOK <145905698+OKCHANGHYEOK@users.noreply.github.com>
2026-07-13 15:44:58 +0900

    [level 1] Title: 기사단원의 무기, Time: 876.28 ms, Memory: 11.3 MB -BaekjoonHub

--- 프로그래머스/1/136798. 기사단원의 무기/README.md
+++ 프로그래머스/1/136798. 기사단원의 무기/README.md
@@ -1,10 +1,10 @@
 # [level 1] 기사단원의 무기 - 136798 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/136798?language=python3) 
 
 ### 성능 요약
 
-메모리: 80.2 MB, 시간: 5.11 ms
+메모리: 11.3 MB, 시간: 876.28 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 02월 24일 21:08:16
+2026년 07월 13일 15:44:53
 
 ### 문제 설명
 
---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.py
@@ -0,0 +1,19 @@
+import math
+
+def solution(number, limit, power):
+    answer = 0
+
+    for i in range(1, number + 1):
+        count = 0
+
+        for j in range(1, math.isqrt(i) + 1):
+            if i % j == 0:
+                count += 1 if i // j == j else 2
+
+            if count > limit:
+                count = power
+                break  
+
+        answer += count        
+
+    return answer
